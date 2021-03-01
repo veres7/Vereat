22b7eebb8b9df5d9f263be913c3b9f2fb7715e98
veres7 <veres7@users.noreply.github.com>
2021-03-01 15:08:32 +0100

    Fix proxy access to API, due to CORS-anywhere restriction

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,7 +3,7 @@ const apiKey = process.env.REACT_APP_KEY1;
 const Yelp = {
   search(term, location, sortBy) {
     return fetch(
-      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      `https://cors.bridged.cc/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
       {
         headers: {
           Authorization: `Bearer ${apiKey}`,
